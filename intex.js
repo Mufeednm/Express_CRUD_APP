c9009db30bcf6dede3c364057c8a2a0d518110a8
mufeednm <mufeedmusthafanm@gmail.com>
2024-05-18 20:48:22 +0530

    competed crud  can add update get detlet as per id

--- intex.js
+++ intex.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT;
 app.get("/",(req,res)=>{
 res.status(200).json(data)
 })
+
 app.post("/user",(req,res)=>{
 
     const {name ,email,username}  =  req.body;
@@ -28,8 +29,47 @@ app.post("/user",(req,res)=>{
             
         }
     })
-res.send("hellsado")
+    res.status(200).json({ message: "Post request received", id });
 })
+app.put("/user/:id",(req,res)=>{
+    const id = req.params.id
+    const finduser = data.findIndex(value => value.id==id)
+    const updateuser=data[finduser]
+
+
+    const {name,email,username}  = req.body;
+
+
+    if(name)updateuser.name=name
+    if(email)updateuser.email=email
+    if(username)updateuser.username=username
+    fs.writeFile("./db/datastore.json",JSON.stringify(data),(error)=>{
+        if (error) {
+            console.log("its error final data",error);
+            
+        }
+    })
+    res.status(200).json({ message: "PUT request received", id });
+    // console.log(finduser);
+    
+})
+app.delete("/user/:id", (req, res) => {
+    const id = req.params.id;
+    const locater = data.findIndex(value => value.id == id);
+
+    if (locater !== -1) {
+        data.splice(locater, 1);
+        fs.writeFile("./db/datastore.json", JSON.stringify(data), (error) => {
+            if (error) {
+                console.log("Error writing final data", error);
+                return res.status(500).json({ message: "Internal Server Error" });
+            }
+            res.status(200).json({ message: "Delete request received", id });
+        });
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
 
 app.listen(PORT,()=>{
     console.log(`server running in ${PORT}`);
